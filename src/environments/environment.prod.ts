aa05bedaf8bafe7458372df14d54e4399625f0dd
KaduFallone <kadufallone@gmail.com>
2026-01-25 15:22:22 -0300

    feat: configurar veriaveis de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBAgjzbCrKccH0ODmkn8HL4EueoyVNfWIQ",
+    authDomain: "socialmediawebsite-a7ace.firebaseapp.com",
+    projectId: "socialmediawebsite-a7ace",
+    storageBucket: "socialmediawebsite-a7ace.firebasestorage.app",
+    messagingSenderId: "391342326065",
+    appId: "1:391342326065:web:71e71798af9fc5dbac7010",
+    measurementId: "G-RFTTZ4NTRJ"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBAgjzbCrKccH0ODmkn8HL4EueoyVNfWIQ",
+    authDomain: "socialmediawebsite-a7ace.firebaseapp.com",
+    projectId: "socialmediawebsite-a7ace",
+    storageBucket: "socialmediawebsite-a7ace.firebasestorage.app",
+    messagingSenderId: "391342326065",
+    appId: "1:391342326065:web:71e71798af9fc5dbac7010",
+    measurementId: "G-RFTTZ4NTRJ",
+    databaseURL: "https://socialmediawebsite-a7ace-default-rtdb.firebaseio.com"
+    
+  }
 };
 
 /*
