6115e1a950e14e79b1d1334ff7b87c781cdf1b54
KaduFallone <kadufallone@gmail.com>
2026-01-29 10:43:14 -0300

    chore: add .gitignore as variaveis de ambiente do environment.ts e environment.prod.ts

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "XXXXXXXXXXX",
+    authDomain: "XXXXXXXX",
+    projectId: "XXXXXXX",
+    storageBucket: "XXXXXXXXXX",
+    messagingSenderId: "XXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXX",
+    measurementId: "XXXXXXX"
+  }
+};
---
+++ src/environments/environment.prod.example.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "XXXXXXX",
+    authDomain: "XXXXXX",
+    projectId: "XXXXXX",
+    storageBucket: "XXXXXXXX",
+    messagingSenderId: "XXXXXX",
+    appId: "XXXXXXXX",
+    measurementId: "XXXXXXX"
+  }
+};
